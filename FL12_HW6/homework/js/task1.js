0b13052e7ae11036c392624343854d3f57dbbe19
vblyahar <vovabliahar@gmail.com>
2020-01-14 18:52:17 +0100

    workUpdates

---
+++ FL12_HW6/homework/js/task1.js
@@ -0,0 +1,32 @@
+// Your code goes here
+const myTask1 =()=>{
+  const a = document.getElementById("a").value
+  const b = document.getElementById("b").value
+  const c = document.getElementById("c").value
+
+  if (typeof a === "number"){
+    console.log("tru",a)
+  }
+  else {
+    console.log("error",a)
+  }
+//   const d = b*b-4*a*c
+//   console.log(`d=${d}`)
+//   if(d<0){
+//     console.log(`no solutio d=${d}`)
+//   }
+//   else if(d===0){
+//     const x=(-b)/(2*a)
+//   console.log(`the solution is one 
+//   d=${d} 
+// and 
+//   x=${x}`)
+//   }
+//   else{
+//     const x1=(-b-Math.sqrt(d))/(2*a)
+//     const x2=(-b+Math.sqrt(d))/(2*a)
+//     console.log(`solution
+//        x1=${x1}
+//        x2=${x2}`)
+//   }
+}
