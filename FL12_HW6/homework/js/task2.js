f8136eead25c5455d01aed17ee5a5ef2b429a1b5
vblyahar <vovabliahar@gmail.com>
2020-01-18 22:15:07 +0100

    complate6Task

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,1 +1,20 @@
-// Your code goes here
+let a = +prompt('Length of the first side');
+let b = +prompt('Length of the second side');
+let c = +prompt('Length of the third side ');
+
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+   console.log('input values should be ONLY numbers');
+} if (a <= 0 || b <= 0 || c <= 0) {
+    console.log('A triangle must have 3 sides with a positive definite length');
+} if (a < b + c && b < a + c && c < a + b) {
+    console.log('Triangle exist.');
+    if (a === b && b === c && c === a) {
+        console.log('Equilateral triangle');
+    } else if (a === b || b === c || a === c) {
+        console.log('Isosceles triangle');
+    } else if (a !== b && b !== c && c !== a) {
+        console.log('Scalene triangle');
+    }
+} else {
+    console.log('Triangle doesn’t exist');
+}
